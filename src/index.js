c1526de38db3036741663f76ced9be7d5846df02
Alisher Begmatov <60242355+alisherbegmatov@users.noreply.github.com>
2021-04-05 02:26:01 -0700

    Challenge 2 Completed
    Challenge 2 Completed

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@
 function capitalize(str) {
     return str[0].toUpperCase() + str.slice(1)
   }
+
+// CHALLENGE 2 - capitalizeWords() - makes all characters uppercase.
+function capitalizeWords(str) {
+    return str.toUpperCase()
+  }
