bfcc9316b2c8e1e7cda7f3a23b9a7acdf9fe6feb
Alisher Begmatov <60242355+alisherbegmatov@users.noreply.github.com>
2021-04-05 02:28:01 -0700

    Challenge 3 Completed
    Challenge 3 Completed

--- src/index.js
+++ src/index.js
@@ -7,3 +7,13 @@ function capitalize(str) {
 function capitalizeWords(str) {
     return str.toUpperCase()
   }
+
+// CHALLENGE 3 - capitalizeHeadline() - makes the first character of each word uppercase. Imagine that each word is separated by a space.
+function capitalizeHeadline(str) {
+    const strArray = str.split(' ')
+    let charArray = []
+    for (let x = 0; x < strArray.length; x++) {
+      charArray.push(strArray[x].charAt(0).toUpperCase() + strArray[x].slice(1));
+    }
+    return charArray.join(" ");
+  }
